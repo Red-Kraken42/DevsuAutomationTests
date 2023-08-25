38a9effc1bf453ae7f9d7616180d9dd690b9a41b
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-24 18:11:06 -0600

    refactoring login from buyProducts test

--- tests/buyProducts.spec.js
+++ tests/buyProducts.spec.js
@@ -1,10 +1,19 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+import loginCredentials from '../utils/loginCredentials.json';
+import { Saucedemo } from '../pageObjects/Saucedemo';
 
 test('Login and Buy Two Products', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.locator('input[data-test="username"]').type('standard_user')
-    await page.locator('input[data-test="password"]').type('secret_sauce')
-    await page.locator('input[data-test="login-button"]').click()
+    // login success
+    const saucedemo = new Saucedemo(page);
+    await saucedemo.goToLoginPage()
+    await saucedemo.submitSuccessLogin(loginCredentials[0].username,loginCredentials[0].password)
+    // Add two products
+    console.log(await page.locator('//div[@class="inventory_item_description"]').count());
+    const elementLocator1 = page.locator('//*[text()="Sauce Labs Backpack"]/ancestor::div[@class="inventory_item_description"]')
+    const elementLocator2 = page.locator('//*[text()="Sauce Labs Onesie"]/ancestor::div[@class="inventory_item_description"]')
+
+    await elementLocator1.locator('//button').click()
+    await elementLocator2.locator('//button').click()
     await page.pause()
 });
