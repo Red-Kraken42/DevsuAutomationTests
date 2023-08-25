389ff02ab9bdf188d9c4bfb0882151934fa2fa52
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-24 20:32:53 -0600

    functions calculateItemPriceSum and extractSubtotalNumber

--- pageObjects/Saucedemo.js
+++ pageObjects/Saucedemo.js
@@ -15,9 +15,10 @@ class Saucedemo {
         this.lastNameField = page.locator('input[data-test="lastName"]')
         this.zipPostalCodeField = page.locator('input[data-test="postalCode"]')
         this.continueBtn = page.locator('input[data-test="continue"]')
+        this.inventoryItemPrices = page.locator('div.inventory_item_price')
 
     }
-    // Login Page
+    // Login Page:
     async goToLoginPage() {
         await this.page.goto("https://www.saucedemo.com/");
     }
@@ -28,18 +29,41 @@ class Saucedemo {
         await this.loginBtn.click()
         await this.page.waitForURL('**/inventory.html')
     }
-    // Inventory Page
+    // Inventory Page:
     async addItemToCart(productName) {
         const elementLocator = this.page.locator(`//*[text()="${productName}"]/ancestor::div[@class="inventory_item_description"]`);
         await elementLocator.locator('//button').click();
     }
 
-    //check out page 1
+    //check out page 1:
     async fillCheckOutInformation(firstName, lastName, zipPostalCode) {
         await this.fistNameField.type(firstName)
         await this.lastNameField.type(lastName)
         await this.zipPostalCodeField.type(zipPostalCode)
         await this.continueBtn.click()
+    }
+    //check out page 2:
+    async calculateItemPriceSum() {
+        let totalPrice = 0;
+        const numItems = await this.inventoryItemPrices.count();
+        for (let i = 0; i < numItems; i++) {
+            const priceText = await this.inventoryItemPrices.nth(i).innerText();
+            this.inventoryItemPrices
+            const priceNumber = parseFloat(priceText.replace('$', ''));
+            totalPrice += priceNumber;
+        }
+        return totalPrice;
+    }
+
+    async extractSubtotalNumber() {
+        const subtotalText = await this.page.locator('div.summary_subtotal_label').innerText();
+        const regex = /\d+\.\d+/; 
+        const matches = subtotalText.match(regex);
+        if (matches) {
+            return parseFloat(matches[0]); 
+        } else {
+            return 0;
+        }
     }
 }
 
