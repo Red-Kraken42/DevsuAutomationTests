8c27c7a8a41d048461c0dd47595651092e424373
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-24 19:56:56 -0600

    additional locators and function fill check out info

--- pageObjects/Saucedemo.js
+++ pageObjects/Saucedemo.js
@@ -6,10 +6,19 @@ class Saucedemo {
         this.usernameField = page.locator('input[data-test="username"]');
         this.passwordField = page.locator('input[data-test="password"]');
         this.loginBtn = page.locator('input[data-test="login-button"]');
+        this.shoppingCartIcon = page.locator('a.shopping_cart_link');
+        this.btn = page.locator('button')
+        this.cartItem = page.locator('div.cart_item')
+        this.itemName = page.locator('.inventory_item_name')
+        this.cartItemNames = page.locator('div.cart_item').locator('.inventory_item_name')
+        this.fistNameField = page.locator('input[data-test="firstName"]')
+        this.lastNameField = page.locator('input[data-test="lastName"]')
+        this.zipPostalCodeField = page.locator('input[data-test="postalCode"]')
+        this.continueBtn = page.locator('input[data-test="continue"]')
+
     }
-
+    // Login Page
     async goToLoginPage() {
-
         await this.page.goto("https://www.saucedemo.com/");
     }
 
@@ -19,9 +28,18 @@ class Saucedemo {
         await this.loginBtn.click()
         await this.page.waitForURL('**/inventory.html')
     }
+    // Inventory Page
     async addItemToCart(productName) {
         const elementLocator = this.page.locator(`//*[text()="${productName}"]/ancestor::div[@class="inventory_item_description"]`);
         await elementLocator.locator('//button').click();
+    }
+
+    //check out page 1
+    async fillCheckOutInformation(firstName, lastName, zipPostalCode) {
+        await this.fistNameField.type(firstName)
+        await this.lastNameField.type(lastName)
+        await this.zipPostalCodeField.type(zipPostalCode)
+        await this.continueBtn.click()
     }
 }
 
