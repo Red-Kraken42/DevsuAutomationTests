86cf1aa998cc29b07831bbbedcf5705b965d53c1
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-24 19:57:34 -0600

    Constants js

---
+++ utils/Constants.js
@@ -0,0 +1,13 @@
+class Constants {
+    static PRODUCT_SAUCE_LABS_BACKPACK = "Sauce Labs Backpack";
+    static PRODUCT_SAUCE_LABS_ONESIE = "Sauce Labs Onesie";
+   
+    static NUMBER_TWO = "2"
+    static productNames = [
+        "Sauce Labs Backpack",
+        "Sauce Labs Onesie",
+      ];
+}
+
+
+module.exports = { Constants };
